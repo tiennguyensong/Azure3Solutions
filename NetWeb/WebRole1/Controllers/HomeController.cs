f6dcfcca2bcc451c46c2f5dcad3e091633d154d5
tien nguyen <tiennguyensong@gmail.com>
2017-08-10 14:46:38 +0700

    Start projects

---
+++ NetWeb/WebRole1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRole1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var c = new CoreProject.Class1();
+            var t = c.Name;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ core/CoreProject/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProject
+{
+    public class Class1
+    {
+        public string Name { get; set; }
+    }
+}
