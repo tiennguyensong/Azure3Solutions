47bf7a7286fb46449582698468f28ccc026b4649
tien nguyen <tiennsloit@gmail.com>
2017-08-11 19:15:56 +0700

    test project with multi build

--- deploy.cmd
+++ deploy.cmd
@@ -3,10 +3,10 @@
 IF "%SITE_FLAVOR%" == "netcore" (
   deploy.netcore.cmd
 ) ELSE (
-  IF "%SITE_FLAVOR%" == "mvc4" (
-    deploy.mvc4.cmd
+  IF "%SITE_FLAVOR%" == "mvc" (
+    deploy.mvc.cmd
   ) ELSE (
-    echo You have to set SITE_FLAVOR setting to either "netcore" or "mvc4"
+    echo You have to set SITE_FLAVOR setting to either "netcore" or "mvc"
     exit /b 1
   )
 )
