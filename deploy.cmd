13767dc5eede03353ba71985815908598d20e927
tien nguyen <tien.nguyen@idealogic.com.vn>
2017-08-11 16:51:49 +0700

    netcore or mvc4

--- deploy.cmd
+++ deploy.cmd
@@ -1,1 +1,12 @@
-<script src="https://gist.github.com/4349245.js"></script>
+@echo off
+
+IF "%SITE_FLAVOR%" == "netcore" (
+  deploy.netcore.cmd
+) ELSE (
+  IF "%SITE_FLAVOR%" == "mvc4" (
+    deploy.mvc4.cmd
+  ) ELSE (
+    echo You have to set SITE_FLAVOR setting to either "netcore" or "mvc4"
+    exit /b 1
+  )
+)
